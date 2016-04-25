5af7a957dc68d0aaea45085a8d1236292509d88b
Shotouke <manuelrojohorno@gmail.com>
2016-04-26 00:26:01 +0200

    Uncomment lines in main.R file

--- main/main.R
+++ main/main.R
@@ -1,5 +1,5 @@
 xmlPath   <- "./inst/extdata/nvdcve-2.0-2016.xml"
-#xmlFile <- readXMLFile(xmlPath)
-#child <- obtenerCPEbyCVE (cpes,"CVE-2016-002")
+xmlFile <- readXMLFile(xmlPath)
+child <- obtenerCPEbyCVE (cpes,"CVE-2016-002")
 
-# cpe <- unlist(obtenerCPEs(xmlFile))
+cpe <- unlist(obtenerCPEs(xmlFile))
