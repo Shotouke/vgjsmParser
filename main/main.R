c1b7a5fc0f56de44ff7e1cc44c8616605feb7079
Master ciberseguridad 2 <master_cs03@a5s104mac31.fib.upc.es>
2016-04-26 20:08:27 +0200

    Actualizar main

--- main/main.R
+++ main/main.R
@@ -1,5 +1,10 @@
 xmlPath   <- "./inst/extdata/nvdcve-2.0-2016.xml"
 xmlFile <- readXMLFile(xmlPath)
-child <- obtenerCPEbyCVE (cpes,"CVE-2016-002")
+#child <- obtencve (xmlFile,"CVE-2016-002")
+#xpath <- "//entry[@id='CVE-2016-0002']/vuln:vulnerable-software-list/vuln:product"
+#doc <- gsub(pattern = "vuln:v", replacement = "vuln_v", x = xmlFile)
+#c<- xpathApply(xmlFile,"//entry[@id='CVE-2016-0002']/vuln:vulnerable-software-list")
+#print(c)
+# cpe <- unlist(obtenerCPEs(xmlFile))
+df <-crear_dataframe(xmlFile)
 
-cpe <- unlist(obtenerCPEs(xmlFile))
